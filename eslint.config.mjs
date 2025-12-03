f1c10fc19f98b579150cdd44c1e77e7d751aeb4d
Julien Marcil <julien@valian.ca>
2025-12-03 12:52:59 -0500

    chore: update project config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { base } from '@valian/eslint-config/base'
 import { importSort } from '@valian/eslint-config/import-sort'
+import { json } from '@valian/eslint-config/json'
 import { node } from '@valian/eslint-config/node'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([...base, ...importSort, ...node, globalIgnores(['packages/'])])
+export default defineConfig([base, importSort, json, node, globalIgnores(['packages/', '.nx/'])])
